04aa963f5c9284273d8b627e24cc3acd907c3330
islam abo mokh <alo.jalal.55@gmail.com>
2023-02-22 17:10:40 +0200

    csac

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,22 @@
 export const locService = {
-    getLocs
+  getLocs,
+  addLoc,
 }
-
 
 const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
 ]
 
-function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs)
-        }, 2000)
-    })
+function addLoc(loc) {
+  locs.push(loc)
+  console.log('hi')
 }
 
-
+function getLocs() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(locs)
+    }, 2000)
+  })
+}
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -1,52 +1,82 @@
-export const mapService = {
-    initMap,
-    addMarker,
-    panTo
-}
+import { locService } from './services/loc.service.js'
 
+export const mapService = {
+  initMap,
+  addMarker,
+  panTo,
+}
 
 // Var that is used throughout this Module (not global)
 var gMap
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap')
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available')
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
-            console.log('Map!', gMap)
-        })
+  console.log('InitMap')
+  return _connectGoogleApi().then(() => {
+    console.log('google available')
+    gMap = new google.maps.Map(document.querySelector('#map'), {
+      center: { lat, lng },
+      zoom: 15,
+    })
+    gMap.addListener('click', (mapsMouseEvent) => {
+      handleMapClick(mapsMouseEvent.latLng.toJSON())
+    })
+    console.log('Map!', gMap)
+  })
+}
+function handleMapClick(ev) {
+  const latitude = ev.lat
+  const langitude = ev.lng
+  const locationName = prompt('What is the name of the location?')
+  const id = makeId()
+  const createdAt = new Date()
+  const updatedAt = createdAt
+
+  const location = {
+    // createdAt,
+    // id,
+    locationName,
+    latitude,
+    langitude,
+    // updatedAt,
+    // weather,
+  }
+  locService.addLoc(location)
 }
 
 function addMarker(loc) {
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
-        title: 'Hello World!'
-    })
-    return marker
+  var marker = new google.maps.Marker({
+    position: loc,
+    map: gMap,
+    title: 'Hello World!',
+  })
+  return marker
 }
 
 function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng)
-    gMap.panTo(laLatLng)
+  var laLatLng = new google.maps.LatLng(lat, lng)
+  gMap.panTo(laLatLng)
 }
 
-
 function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
-    var elGoogleApi = document.createElement('script')
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
-    elGoogleApi.async = true
-    document.body.append(elGoogleApi)
+  if (window.google) return Promise.resolve()
+  const API_KEY = '' //TODO: Enter your API Key
+  var elGoogleApi = document.createElement('script')
+  elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
+  elGoogleApi.async = true
+  document.body.append(elGoogleApi)
 
-    return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
+  return new Promise((resolve, reject) => {
+    elGoogleApi.onload = resolve
+    elGoogleApi.onerror = () => reject('Google script failed to load')
+  })
+}
+
+function makeId(length = 6) {
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = ''
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
 }
